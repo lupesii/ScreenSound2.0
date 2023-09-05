c4c2b4154f94f1b8874d9ca3b3b4dc0e5b0626f6
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-09-05 17:46:56 -0300

    Criando método da classe

--- ScreenSound2.0/Classe-Musica.cs
+++ ScreenSound2.0/Classe-Musica.cs
@@ -4,4 +4,19 @@
     public string nome;
     public int duracao;
     public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if(disponivel)
+        {
+            Console.WriteLine("Disponivel no plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Ultra");
+        }
+    }
 }
--- ScreenSound2.0/Program.cs
+++ ScreenSound2.0/Program.cs
@@ -1,6 +1,14 @@
 ﻿Musica musica1 =  new Musica();
 musica1.nome = "Stressed Out";
 musica1.artista = "Twenty One Pilots";
+musica1.duracao = 300;
+musica1.disponivel = true;
 
-Console.WriteLine(musica1.nome);
-Console.WriteLine(musica1.artista);
+Musica musica2 = new Musica();
+musica2.nome = "Bem melhor";
+musica2.artista = "Mayis";
+musica2.duracao = 294;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();

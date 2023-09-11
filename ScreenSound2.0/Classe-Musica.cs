df5c4a84ca8c5cf058e8f37b216da29f210be516
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-09-11 17:18:40 -0300

    Utilizando Propeties

--- ScreenSound2.0/Classe-Musica.cs
+++ ScreenSound2.0/Classe-Musica.cs
@@ -3,24 +3,14 @@
     public string artista;
     public string nome;
     public int duracao;
-    private bool disponivel;
-
-    public void EscreveDisponivel(bool value)
-    {
-        disponivel = value;
-    }
-
-    public bool LeDisponivel()
-    {
-        return disponivel;
-    }
+    public bool Disponivel { get; set; }
 
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {nome}");
         Console.WriteLine($"Artista: {artista}");
         Console.WriteLine($"Duração: {duracao}");
-        if(disponivel)
+        if(Disponivel)
         {
             Console.WriteLine("Disponivel no plano");
         }

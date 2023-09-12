66f7d575f57ba4e475f4951b3aae9ad34cdc577f
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2023-09-12 16:57:50 -0300

    Criação da classe Album e método de AdcicionarMusica

---
+++ ScreenSound2.0/Album.cs
@@ -0,0 +1,10 @@
+﻿class Album
+{
+    private List<Musica> Musicas = new List<Musica>();
+    public string Nome { get; set; }
+
+    public void AdicionarMusica(Musica musica)
+    {
+        Musicas.Add(musica);
+    }
+}
